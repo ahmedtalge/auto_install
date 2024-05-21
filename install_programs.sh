b956fa024083537383a60e769fee7e5012dc1897
Ahmed Talge <101481879+ahmedtalge@users.noreply.github.com>
2024-05-20 23:39:01 -0300

    Update install_programs.sh

--- install_programs.sh
+++ install_programs.sh
@@ -35,10 +35,30 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 # Instalar programas via Flatpak
 flatpaks=(
-  "org.flameshot.Flameshot"
-  "io.github.jackulu.localsend_app"
-  "com.github.unrud.VideoDownloader"
-  "org.keepassxc.KeePassXC"
+    "md.obsidian.Obsidian"
+    "com.todoist.Todoist"
+    "io.github.brunofin.Cohesion"
+    "io.github.mimbrero.WhatsAppDesktop"
+    "org.telegram.desktop"
+    "org.telegram.desktop.webview"
+    "com.github.unrud.VideoDownloader"
+    "io.github.giantpinkrobots.varia"
+    "org.flameshot.Flameshot"
+    "org.kde.okular"
+    "org.keepassxc.KeePassXC"
+    "org.libreoffice.LibreOffice"
+    "org.localsend.localsend_app"
+    "com.spotify.Client"
+    "org.gimp.GIMP"
+    "org.inkscape.Inkscape"
+    "com.github.vikdevelop.photopea_app"
+    "org.upscayl.Upscayl"
+    "org.videolan.VLC"
+    "org.prismlauncher.PrismLauncher"
+    "io.github.flattool.Warehouse"
+    "com.github.tchx84.Flatseal"
+    "io.github.giantpinkrobots.flatsweep"
+    " io.github.prateekmedia.appimagepool"
 )
 
 for flatpak in "${flatpaks[@]}"; do
@@ -47,16 +67,26 @@ done
 
 # Instalar programas essenciais
 essentials=(
-  "curl"
-  "git"
-  "vim"
-  "gnome-tweaks"
-  "build-essential"
-  "ubuntu-restricted-extras"
+    "mint-meta-codecs"
+    "curl"
+    "git"
+    "htop"
+    "vim"
+    "build-essential"
+    "ubuntu-restricted-extras"
+    "ttf-mscorefonts-installer"
+    "q4wine"
 )
 
 sudo apt-get update
 sudo apt-get install -y "${essentials[@]}"
+
+# Instalação do Wine Staging
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key -O- | sudo apt-key add -
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ jammy main'
+sudo apt-get update
+sudo apt-get install -y --install-recommends winehq-staging
 
 # Limpeza do sistema
 sudo apt-get autoremove -y
